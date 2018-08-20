bce4679c406f3ee0bab585615c9efa409e81a810
AsifRehman <asif.pro@gmail.com>
2018-08-20 15:58:30 +0500

    Add vue PartyCategs

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Partytypes from './views/PartyTypes.vue'
+import PartyCategs from './views/PartyCategs.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,7 @@ export default new Router({
   routes: [
     { path: '/', name: 'home', component: Home},
     { path: '/partytypes', component: Partytypes},
+    { path: '/partyCategs', component: PartyCategs},
     { path: '/about', name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
--- src/views/PartyTypes.vue
+++ src/views/PartyTypes.vue
@@ -1,36 +1,61 @@
 <template>
-  <v-card class="hide-overflow" color="purple lighten-1" dark>
-    <v-toolbar card color="purple">
-      <v-icon>mdi-account</v-icon>
-      <v-toolbar-title class="font-weight-light">Party Types</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn color="purple darken-3" fab small @click="isEditing = !isEditing">
-        <v-icon v-if="isEditing">mdi-close</v-icon>
-        <v-icon v-else>mdi-pencil</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-card-text>
-      <v-text-field v-model="PartyTypeID" :disabled="!isEditing" color="white" label="PartyType ID"></v-text-field>
-      <v-text-field v-model="PartyType" :disabled="!isEditing" color="white" label="PartyType"></v-text-field>
-      <v-autocomplete v-model="PartyCategID" :disabled="!isEditing" :items="Categories" :filter="customFilter" color="white" item-text="name" label="Party Catgory"></v-autocomplete>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn :disabled="!isEditing" color="success" @click="save">Save</v-btn>
-    </v-card-actions>
-    <v-snackbar v-model="hasSaved" :timeout="2000" absolute bottom left>Your profile has been updated</v-snackbar>
-  </v-card>
+  <div>
+    <v-btn color="success" @click="get">Get</v-btn>
+
+    <v-card class="hide-overflow" color="purple lighten-1" dark>
+      <v-toolbar card color="purple">
+        <v-icon>mdi-account</v-icon>
+        <v-toolbar-title class="font-weight-light">Party Types</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="purple darken-3" fab small @click="isEditing = !isEditing">
+          <v-icon v-if="isEditing">mdi-close</v-icon>
+          <v-icon v-else>mdi-pencil</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <v-text-field v-model="PartyTypeID" :disabled="!isEditing" color="white" label="PartyType ID"></v-text-field>
+        <v-text-field v-model="PartyType" :disabled="!isEditing" color="white" label="PartyType"></v-text-field>
+        <v-autocomplete v-model="PartyCategID" :disabled="!isEditing" :items="Categories" :filter="customFilter" color="white" item-text="name" label="Party Catgory"></v-autocomplete>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn :disabled="!isEditing" color="success" @click="save">Save</v-btn>
+      </v-card-actions>
+      <v-snackbar v-model="hasSaved" :timeout="2000" absolute bottom left>Your profile has been updated</v-snackbar>
+    </v-card>
+    <v-data-table :headers="headers" :items="PartyTypes" class="elevation-1">
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.id }}</td>
+        <td class="text-xs-left">{{ props.item.PartyType }}</td>
+        <td class="text-xs-centre">{{ props.item.UPartyType }}</td>
+        <td class="text-xs-centre">{{ props.item.PartyCategID }}</td>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
     data () {
       return {
+        headers: [
+          {
+            text: 'PartyTypeID',
+            align: 'left',
+            value: 'name'
+          },
+          { text: 'PartyType', value: 'partytype' },
+          { text: 'UPartyType', value: 'Upartytype' },
+          { text: 'PartyCategID', value: 'PartyCategID' }
+        ],
+        PartyTypes: [],
         PartyTypeID: null,
         PartyType: '',
         PartyCategID: null,
-
+        url: 'http://localhost/odata/tbl_PartyType',
         hasSaved: false,
         isEditing: true,
         model: null,
@@ -46,9 +71,6 @@
 
     methods: {
       customFilter (item, queryText, itemText) {
-        console.log(queryText);
-        console.log(itemText);
-
         const textOne = item.name.toLowerCase()
         const textTwo = item.abbr.toLowerCase()
         const searchText = queryText.toLowerCase()
@@ -58,7 +80,30 @@
       },
       save () {
         this.isEditing = !this.isEditing
+        axios.post(this.url, {"id": this.PartyTypeID, "PartyType": this.PartyType, "PartyCategID": this.PartyCategID})
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function (error){
+          console.log(error);
+        });
         this.hasSaved = true
+      },
+      mounted() {
+        this.get();
+        console.log('xyz');
+      },
+      get () {
+        console.log('get');
+        var vm = this;
+        axios.get(this.url, {"content-type" : "application/json"})
+        .then(function(response){
+          vm.PartyTypes = response.data.value;
+          //console.log(response.data.value);
+        })
+        .catch(function(error){
+          console.log(error);
+        });
       }
     }
   }
