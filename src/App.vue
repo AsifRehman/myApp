443c5aaf1eda8affb1c9884553189d6a0926c4ef
AsifRehman <asif.pro@gmail.com>
2018-09-01 12:39:46 +0500

    First Changes

--- src/App.vue
+++ src/App.vue
@@ -1,136 +1,26 @@
 <template>
   <v-app>
+    <v-alert type="accent" :value="true" >
+      {{title}}
+    </v-alert>
+    <v-toolbar color="accent">
 
-<v-navigation-drawer
-    stateless
-    value="true"
-    persistent
-    :mini-variant="miniVariant"
-    :clipped="clipped"
-    v-model="drawer"
-    enable-resize-watcher
-    fixed
-    app
-  >
-    <v-list>
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon>home</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>Home</v-list-tile-title>
-      </v-list-tile>
-
-      <v-list-group
-        prepend-icon="account_circle"
-        value="true"
-      >
-        <v-list-tile slot="activator">
-          <v-list-tile-title>Users</v-list-tile-title>
-        </v-list-tile>
-
-        <v-list-group
-          no-action
-          sub-group
-          value="true"
-        >
-          <v-list-tile slot="activator">
-            <v-list-tile-title>Admin</v-list-tile-title>
-          </v-list-tile>
-
-          <v-list-tile
-            v-for="(admin, i) in admins"
-            :key="i"
-            @click="LoadPage(admin)"
-          >
-            <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
-            <v-list-tile-action>
-              <v-icon v-text="admin[1]"></v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list-group>
-
-        <v-list-group
-          sub-group
-          no-action
-        >
-          <v-list-tile slot="activator">
-            <v-list-tile-title>Actions</v-list-tile-title>
-          </v-list-tile>
-
-          <v-list-tile
-            v-for="(crud, i) in cruds"
-            :key="i"
-            @click="LoadPage(crud)"
-          >
-            <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
-            <v-list-tile-action>
-              <v-icon v-text="crud[1]"></v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list-group>
-      </v-list-group>
-    </v-list>
-  </v-navigation-drawer>
-
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
-    <v-content>
-      <v-fade-transition mode="out-in">
-        <router-view></router-view>
-      </v-fade-transition>
-    </v-content>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      admins: [
-        ['PartyCateg', 'people_outline'],
-        ['PartyType', 'settings']
-      ],
-      cruds: [
-        ['PartyCateg', 'add'],
-        ['Read', 'insert_drive_file'],
-        ['Update', 'update'],
-        ['Delete', 'delete']
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      title: "Vuetify.js"
+    };
   },
   methods: {
     LoadPage(data) {
@@ -139,5 +29,5 @@ export default {
       //console.log(this.$router);
     }
   }
-}
+};
 </script>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Partytypes from './views/PartyTypes.vue'
-import PartyCategs from './views/PartyCategs.vue'
+
+// accounts
+import Partytypes from './views/accounts/partytypes/PartyTypes.vue'
+import PartyCategs from './views/accounts/partycategs/PartyCategs.vue'
+
+
 
 Vue.use(Router)
 
@@ -17,7 +21,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/About/About.vue')
     }
   ]
 })
