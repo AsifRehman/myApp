eb3ea6e72020aac7e244d3e241cd85929e8783ab
Asif Rehman <asif.pro@gmail.com>
2019-12-01 15:59:54 +0500

    cp form further matured

--- src/views/transactions/CashPayment/cp.vue
+++ src/views/transactions/CashPayment/cp.vue
@@ -1,67 +1,98 @@
 <template>
   <div class="about">
-    <label> {{ Status }} </label>
-    {{ items.MHeadID }}
-    {{ items.MHead }}
-    <input type="text" v-model="items.MHeadID" placeholder="MHeadID" >
-    <input type="text" v-model="items.MHead" placeholder="MHead" >
-    <input type="button" @click="Add" value="Post"/>
+    <label>{{ Status }}</label>
+    <v-text-field v-model="editedItem.MHeadID" ref="MHeadID" label="MHead ID" @keyup.enter="this.$refs.MHead.focus()"></v-text-field>
+    <v-text-field v-model="editedItem.MHead" ref="MHead" label="MHead"></v-text-field>
+    <v-btn color="success" @click="Add">Post</v-btn>
     <h1>cash payment</h1>
-    <div v-for="v in mainList" :key="v.MHeadID">
-        {{ v.MHeadID }}
-        {{ v.MHead }}
-    </div>
-    <div v-show="loading">
-        Loading...
-    </div>
+
+    <v-data-table :headers="headers" :items="mainList" hide-actions class="elevation-1">
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.MHeadID }}</td>
+        <td class="text-xs-centre">{{ props.item.MHead }}</td>
+        <td class="justify-center layout px-0">
+          <v-icon small @click="Delete(props.item)">delete</v-icon>
+        </td>
+      </template>
+    </v-data-table>
+
+    <div v-show="loading">Loading...</div>
   </div>
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      url: '',
+      url: "",
       mainList: [],
       loading: true,
-      items: {
-        MHeadID: '',
-        MHead: ''
+      headers: [
+        {
+          text: 'MHeadID',
+          align: 'left',
+          sortable: true,
+          value: 'MHeadID'
+        },
+        { text: 'MHead', value: 'MHead' }],
+      editedItem: {
+        MHeadID: "",
+        MHead: ""
       },
-      Status: ''
-    }
+      Status: ""
+    };
   },
-  created () {
-    this.initialize()
+  created() {
+    this.initialize();
   },
   methods: {
-    initialize () {
-      this.url = this.$store.state.url + 'tbl_MHead';
+    initialize() {
+      this.url = this.$store.state.url + "tbl_MHead";
       var vm = this;
-      axios.get( this.url, { "content-type": "application/json"})
-      .then(function(response){
+      axios
+        .get(this.url, { "content-type": "application/json" })
+        .then(function(response) {
           vm.mainList = response.data.value;
           vm.loading = false;
-      })
-      .catch(function(error){
-        // eslint-disable-next-line
-        Console.log(error);
-      })
+        })
+        .catch(function(error) {
+          // eslint-disable-next-line
+          Console.log(error);
+        });
     },
-    Add(){
+    Reset() {
+      this.editedItem.MHeadID = null;
+      this.editedItem.MHead = "";
+    },
+    Add() {
+      debugger
       var vm = this;
-      var vMHeadID = vm.items.MHeadID;
-      var vMHead = vm.items.MHead;
-      
+      var item = { ...this.editedItem}
+      this.Reset();
+      vm.$refs.MHeadID.focus();
 
-      axios.post(this.url, {MHeadID: vMHeadID, MHead: vMHead})
-      .then(function(response){
+      axios.post(this.url, item).then(function(response) {
+        vm.Status = response.config.data;
+        vm.mainList.push(JSON.parse(response.config.data));
+      });
+    },
+    Delete(item) {
+      //debugger
+      var vm = this;
+      const index = vm.mainList.indexOf(item);
+      this.mainList.splice(index, 1);
+      axios
+        .delete(this.url + "(" + item.MHeadID + ")")
+        .then(function(response) {
           vm.Status = response;
-      })
+        })
+        .catch(function(response) {
+          vm.Status = response;
+          vm.mainList.push(item);
+        });
     }
   }
-}
+};
 </script>
